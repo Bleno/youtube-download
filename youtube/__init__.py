833a7806e9df4fc8c0732e1b6a6229e7e47f072c
Bleno <blenobok@gmail.com>
2017-06-08 14:39:38 -0300

    It's works

--- youtube/__init__.py
+++ youtube/__init__.py
@@ -8,5 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('youtube-dl', '/youtube-dl')
+    config.add_route('download', '/download/{name}')
     config.scan()
     return config.make_wsgi_app()
--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,32 @@
 from pyramid.view import view_config
+from pyramid.response import Response
+import youtube_dl
+import uuid
+import os
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='templates/home.jinja2')
 def my_view(request):
-    return {'project': 'youtube'}
+    videos = [ f  for f in os.listdir('/tmp') if f.endswith('.mp4')]
+    return {'project': 'youtube', 'videos': videos }
+
+
+
+@view_config(route_name='youtube-dl', renderer='json', request_method=['POST'])
+def get_youtube(request):
+    url = request.params['url']
+    out_dir = '/tmp'
+    name = uuid.uuid4().hex
+    print(name)
+    #name = 'git'
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%s/%%(title)s.%%(ext)s' % out_dir})
+    y = ydl.extract_info(url) # aqui colocar a url
+    name = y['title']
+    return [ f  for f in os.listdir('/tmp') if f.startswith(name)]
+
+
+@view_config(route_name='download')
+def download(request):
+    name = request.matchdict['name']
+    _file = open('/tmp/' + name, 'rb')
+    return Response(_file.read(), content_type="application/octet-stream")
